7836fc4fc2299fb0734f81ef1eb1b147558c8bf1
Caitlin Tibbetts <caitlin@tibbettsfamily.com>
2022-07-03 16:24:53 -0400

    Simplified home page even more

--- src/App.js
+++ src/App.js
@@ -1,71 +1,60 @@
-import React, { useState } from "react";
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 
-import db from './firebase';
-import './App.css';
+import db from "./firebase";
+import "./App.css";
 
 function App() {
-  const [joinGameClicked, setJoinGameClicked] = useState(false);
-
-  if(joinGameClicked) {
-    return (
-      <div className="App">
-        <div className="Floating-form">
-          <h1>Welcome to the Scavenger Hunt!</h1>
-          <h2>Mequet Family Dauphin Island 2022</h2>
-          <Formik
-            initialValues={{ teamName: '', password: '', gamePin: '' }}
-            validate={values => {
-              const regex = new RegExp('[0-9]{4}$');
-              const errors = {};
-              if (!values.teamName) {
-                errors.teamName = 'Required';
-              }
-              if (!values.gamePin || !regex.test(values.gamePin)) {
-                errors.gamePin = 'Game pin must be exactly four numbers';
-              }
-              return errors;
-            }}
-            onSubmit={(values, { setSubmitting }) => {
-              db.collection("/team").where('teamName', '==', 'Test').then((res) => {
+  return (
+    <div className="App">
+      <div className="Floating-form">
+        <h1>Welcome to the Scavenger Hunt!</h1>
+        <h2>Mequet Family Dauphin Island 2022</h2>
+        <Formik
+          initialValues={{ teamName: "", password: "", gamePin: "" }}
+          validate={(values) => {
+            const regex = new RegExp("[0-9]{4}$");
+            const errors = {};
+            if (!values.teamName) {
+              errors.teamName = "Required";
+            }
+            if (!values.gamePin || !regex.test(values.gamePin)) {
+              errors.gamePin = "Game pin must be exactly four numbers";
+            }
+            return errors;
+          }}
+          onSubmit={(values, { setSubmitting }) => {
+            db.collection("/team")
+              .where("teamName", "==", "Test")
+              .then((res) => {
                 console.log(res.data());
-              }).catch((err) => {
+              })
+              .catch((err) => {
                 console.log(err);
               });
-            }}
-          >
-            {({ isSubmitting }) => (
-              <Form>
-                <p>Team Name: <Field name="teamName" /></p>
-                <ErrorMessage name="teamName" component="div" />
+          }}
+        >
+          {({ isSubmitting }) => (
+            <Form>
+              <p>
+                Team Name: <Field name="teamName" />
+              </p>
+              <ErrorMessage name="teamName" component="p" />
 
-                <p>Game Pin: <Field name="gamePin"/></p>
-                <ErrorMessage name="gamePin" component="div" />
+              <p>
+                Game Pin: <Field name="gamePin" />
+              </p>
+              <ErrorMessage name="gamePin" component="p" />
 
-                <button type="submit" disabled={isSubmitting}>
-                  Join Now!
-                </button>
-              </Form>
-            )}
-          </Formik>
-        </div>
+              <button type="submit" disabled={isSubmitting}>
+                Join Now!
+              </button>
+            </Form>
+          )}
+        </Formik>
       </div>
-    );
-  } else {
-    return (
-      <div className="App">
-        <div className="Floating-form">
-          <h1>Welcome to the Scavenger Hunt!</h1>
-          <h2>Mequet Family Dauphin Island 2022</h2>
-          <button onClick={() => {
-            setJoinGameClicked(true);
-          }}>
-            Join a Game!
-          </button>
-        </div>
-      </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
