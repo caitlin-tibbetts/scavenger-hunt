0ab765618136116f0dde3139f2ed204dbfdd7101
Gabe Goldstein <thegabe6@yahoo.com>
2022-07-04 01:20:55 -0400

    Added Task View

--- src/Task.js
+++ src/Task.js
@@ -1,18 +1,50 @@
 import { Typography } from "@material-ui/core";
-import React from "react";
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import React, { useState } from "react";
 
 import "./Task.css";
 
-
 function Task(props) {
-    return (
-        <div className="Game">
-            <Typography>
-                {props.desc}
-            </Typography>
-        </div>
-    )
+  const [incorrect, setIncorrect, incorrectRef] = useState(false);
 
+  return (
+    <div className="Task">
+      <Typography>{props.desc}</Typography>
+
+      <Formik
+        initialValues={{ answer: "" }}
+        validate={(values) => {
+          const errors = {};
+          if (!values.answer) {
+            errors.answer = "Required";
+          }
+          if (values.answer !== props.answer) {
+            errors.answer = "Incorrect answer. Please try again.";
+          }
+
+          return errors;
+        }}
+        onSubmit={async (values, { setSubmitting }) => {
+          if (values.answer !== props.answer) {
+            setIncorrect(true);
+          }
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <p>
+              Answer: <Field name="answer"/>
+            </p>
+            <ErrorMessage name="answer" />
+            {incorrectRef && <div>Incorrect Answer</div>}
+            <button type="submit" disabled={isSubmitting}>
+              Submit
+            </button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
 }
 
 export default Task;
