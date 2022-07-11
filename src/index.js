f91523149716b2ea497844830e0540ec433eb426
Caitlin Tibbetts <caitlin@tibbettsfamily.com>
2022-07-11 18:29:55 -0400

    Missed a file, now it's all refactored

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./style/index.css";
-import App from "./App";
-import Admin from "./routes/Admin";
-import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { CookiesProvider } from "react-cookie";
+
+import "./style/index.css";
+
+import App from "./App";
+import Admin from "./routes/Admin";
 import Dashboard from "./routes/Dashboard";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -20,8 +21,3 @@ root.render(
     </BrowserRouter>
   </CookiesProvider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
