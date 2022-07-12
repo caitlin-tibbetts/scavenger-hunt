aef5d720d198b1c02f2a9e29413f34ba3dd3d0c4
Caitlin Tibbetts <caitlin@tibbettsfamily.com>
2022-07-11 22:57:39 -0400

    Last bit of formatting and refactoring

--- src/components/ScoreList.js
+++ src/components/ScoreList.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Grid from "@material-ui/core/Grid";
 import ReactLoading from "react-loading";
-import { collection, onSnapshot} from "firebase/firestore";
+import { collection, onSnapshot } from "firebase/firestore";
 
 import "../style/App.css";
 import "../style/Leaderboard.css";
@@ -10,44 +10,39 @@ import db from "../firebase";
 import ScoreCard from "./ScoreCard";
 
 function ScoreList(props) {
+  const [teamData, setTeamData] = useState();
+  const [currentPage, setCurrentPage] = React.useState(0);
 
-    const [teamData, setTeamData] = useState();
-    const [currentPage, setCurrentPage] = React.useState(0);
-
-    useEffect(() => {
-      const timerId = setInterval(() => {
-      
-        setCurrentPage(cur => teamData  ? (cur < (teamData.length)  % 6 - 1 ? cur + 1 : 0) : 0);
+  useEffect(() => {
+    const timerId = setInterval(() => {
+      setCurrentPage((cur) =>
+        teamData ? (cur < (teamData.length % 6) - 1 ? cur + 1 : 0) : 0
+      );
     }, 7000);
     return () => {
-      
-        clearInterval(timerId);
-        
+      clearInterval(timerId);
     };
+  }, []);
 
-    }, []);
-
-    useEffect(() => {
-          const unsubscribeTeams = onSnapshot(
-            collection(db, "games", props.gamePin, "teams"),
-            (snapshot) => {
-                if (snapshot.size) {
-                  
-                    setTeamData(
-                      snapshot.docs.map((document) => {
-                        return document.data();
-                      }).sort((a,b)=> b.points-a.points)
-                    );
-                  }
-                }
+  useEffect(() => {
+    const unsubscribeTeams = onSnapshot(
+      collection(db, "games", props.gamePin, "teams"),
+      (snapshot) => {
+        if (snapshot.size) {
+          setTeamData(
+            snapshot.docs
+              .map((document) => {
+                return document.data();
+              })
+              .sort((a, b) => b.points - a.points)
           );
-          return () => {
-            
-            unsubscribeTeams();
-            
-          };
-      }, [props.gamePin]);
-    
+        }
+      }
+    );
+    return () => {
+      unsubscribeTeams();
+    };
+  }, [props.gamePin]);
 
   return (
     <>
@@ -61,22 +56,21 @@ function ScoreList(props) {
         spacing={2}
         alignItems="center"
         style={{ flexWrap: "nowrap", overflow: "auto", textAlign: "left" }}
-       
       >
-        {teamData  ? (
+        {teamData ? (
           teamData.map((teamInfo, i) => {
             return (
-              i >= currentPage * 6 && i < (currentPage+1) * 6 &&
-              <Grid item key={i + 1} xs={12} >
-                <ScoreCard
-                  key={teamInfo.id}
-                  teamName={teamInfo.name}
-                  index={i + 1}
-                  points={teamInfo.points}
-                />
-                  </Grid>
-             
-            
+              i >= currentPage * 6 &&
+              i < (currentPage + 1) * 6 && (
+                <Grid item key={i + 1} xs={12}>
+                  <ScoreCard
+                    key={teamInfo.id}
+                    teamName={teamInfo.name}
+                    index={i + 1}
+                    points={teamInfo.points}
+                  />
+                </Grid>
+              )
             );
           })
         ) : (
